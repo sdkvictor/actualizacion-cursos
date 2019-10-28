e96ca5462e5dc47be753998b52325c8e047a60c3
Josecisneros001 <joseacisnerosm@gmail.com>
2019-10-28 17:39:15 -0600

    Enter

---
+++ cursos/Habilidades Básicas para iniciar un negocio/servicio/src/modulo1/tema2/continue7.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html>
+<?php $root="../../../";?>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, shrink-to-fit=no">
+        <title>Tema 1: Consideraciones para utilizar el control biológico | CVA - Centro Virtual de Aprendizaje</title>
+
+        <!-- Favicon -->
+        <link rel="shortcut icon" href="../../../assets/images/logo/favicon.png">
+
+        <!-- plugins css -->
+        <link rel="stylesheet" href="../../../assets/vendors/bootstrap/dist/css/bootstrap.css" />
+        <link rel="stylesheet" href="../../../assets/vendors/PACE/themes/blue/pace-theme-minimal.css" />
+        <link rel="stylesheet" href="../../../assets/vendors/perfect-scrollbar/css/perfect-scrollbar.min.css" />
+
+        <!-- page plugins css -->
+
+        <!-- core css -->
+        <link href="../../../assets/css/ei-icon.css" rel="stylesheet">
+        <link href="../../../assets/css/themify-icons.css" rel="stylesheet">
+        <link href="../../../assets/css/font-awesome.min.css" rel="stylesheet">
+        <link href="../../../assets/css/animate.min.css" rel="stylesheet">
+        <link href="../../../assets/css/app.css" rel="stylesheet">
+        <link href="../../../assets/css/progress.css" rel="stylesheet">
+        <link href="../../../assets/css/progress-circle.css" rel="stylesheet">
+
+    </head>
+
+    <body class="header-info side-nav-dark">
+        <div class="app">
+            <div class="layout">
+                <?php include $root."_resources/recycled/sideNav.html";?>
+                <!-- Page Container START -->
+                <div class="page-container">
+                 <?php include $root."_resources/recycled/header.html";?>
+                
+                      <!-- Content Wrapper START -->
+                      <div class="main-content">
+                            <div class="container-fluid">
+
+
+                                <!-- Your content goes here -->
+                                <!--
+                                <div class="row">
+                                    <div class="col">
+                                        <div class="card">
+                                            <div class="card-horizon">
+                                                <div class="image-container col-md-4">
+                                                    <div class="background-holder has-content" style="background-image: url('../../../assets/images/course/modulo1/consideraciones-control-biologico/shutterstock_538682752.jpg')"></div>
+                                                </div>
+                                                <div class="container-fluid">
+                                                    <div class="row">
+                                                        <div class="col-md-7 ml-auto">
+                                                            <div class="pdd-vertical-30 pdd-horizon-25">
+                                                                <h1 class="mrg-btm-20 card-title">Consideraciones para utilizar el control biológico</h1>
+                                                                <p>Una vez que se decide poner en práctica el control biológico se debe decidir cual estrategia de control biológico podemos utilizar. Las estrategias son las siguientes:</p>
+                                                                <ol>
+                                                                    <li><b>Conservar y acrecentar</b> los parasitoides y depredadores ya presentes en la parcela o el ambiente agrícola.</li>
+                                                                    <br>
+                                                                    <li><b>Importar</b> (de lugares lejanos o diferentes) parasitoides y depredadores, para que éstos se establezcan en nuestras parcelas o ambientes agrícolas. La importación de enemigos naturales de plagas la realizan sólo los organismos gubernamentales y las instituciones de educación superior, por lo que no abundaremos en ella.</li>
+                                                                    <br>
+                                                                    <li><b>Adquirir</b> parasitoides, depredadores y patógenos multiplicados masivamente en centros o laboratorios de reproducción de organismos benéficos y liberarlos o aplicarlos en nuestros cultivos.</li>
+                                                                </ol>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                -->
+                                <div class="row">
+                                    <div class="col">
+                                        <div class="card">
+                                            <div class="card-horizon">
+                                                <div class="image-container col-md-4">
+                                                    <div class="background-holder has-content" style="background-image: url('../../assets/images/course/modulo3/guia-reforestacion/shutterstock_467268155.jpg')"></div>
+                                                </div>
+                                                <div class="container-fluid">
+                                                    <div class="row">
+                                                        <div class="col-md-7 ml-auto">
+                                                            <div class="pdd-vertical-30 pdd-horizon-25">
+                                                                <h1 style="weight:bold" class="mrg-btm-20 card-title"> 2.3 Clases de Empresa </h1>
+                                                                <p>Usted ha definido detalladamente la idea de producto o servicio que desarrollará; es importante que tenga en mente qué clase de empresa tendrá, es decir, su empresa puede ser:</p>
+                                                                <ol>
+                                                                    <li><b>Industrial.</b> Cuando se transforma algún material en un producto terminado útil, se dice que su empresa es de tipo industrial, por ejemplo: procesar fruta para elaborar fruta en conserva, trabajar material de vidrio para elaborar vasos y floreros, elaborar ropa a partir de telas, o bien, hacer canastas a partir de ixtle para venderlas</li>
+                                                                </ol>
+                                                            </div>
+
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        
+                                    </div>
+                                </div>
+                               
+                             
+                             
+                                <div class="card widget-feed">
+                                    <div class="row">
+                                        <div class="col-lg-8">
+                                            <h1 class="feed-title tag tag-info">Tema 1</h1>
+                                            <h3 class="feed-subtitle">Tu Progreso</h3>
+                                            <div class="progress" style="height: 20px;" id="bar">
+                                                <div class="progress-bar bg-info" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" style="max-width: 15%" >15%</div>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-4">
+                                            <div class="row">
+                                                <div class="col">
+                                                    <div id="circlecontainer"></div>
+                                                </div>
+                                            </div>
+                                            <div class="row">
+                                                <div class="col text-center">
+                                                    <h3 class="feed-subtitle">Progreso General del Curso</h3>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col text-center">
+                                        <div class="btn-group">
+                                            <a class="btn btn-default" role="button" href="./continue6.php">
+                                                <span>Atrás</span>
+                                            </a>
+                                            <a class="btn btn-default" role="button" href="./continue8.php">
+                                                <span>Siguiente</span>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                      </div>
+                      <!-- Content Wrapper END -->
+
+                      <?php include_once($root."_resources/recycled/footer.html")?>
+             
+                    </div>
+                    <!-- Page Container END -->
+                </div>
+            </div>
+
+        <!-- build:js assets/js/vendor.js -->
+        <!-- plugins js -->
+        <script src="../../../assets/vendors/jquery/dist/jquery.min.js"></script>
+        <script src="../../../assets/vendors/popper.js/dist/umd/popper.min.js"></script>
+        <script src="../../../assets/vendors/bootstrap/dist/js/bootstrap.js"></script>
+        <script src="../../../assets/vendors/PACE/pace.min.js"></script>
+        <script src="../../../assets/vendors/perfect-scrollbar/js/perfect-scrollbar.jquery.js"></script>
+
+        <!-- Progress plugins -->
+        <script src="../../../assets/js/progressbar.js"></script>
+        <script src="../../../assets/js/progress-circle.js"></script>
+
+        <!-- endbuild -->
+
+        <!-- build:js assets/js/app.min.js -->
+        <!-- core js -->
+        <script src="../../../assets/js/app.js"></script>
+
+
+        <!-- Aquí se inserta el porcentage del círculo de progreso como una fracción -->
+        <script type="text/javascript">show(0.20)</script>
+
+        <!-- endbuild -->
+
+        <!-- Insert your dependencies here -->
+
+    </body>
+</html>
